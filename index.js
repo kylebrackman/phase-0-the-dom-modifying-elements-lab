23afa1db040e47a35e259289f653ac18800ce7dc
Kyle Brackman <kyle.a.brackman@gmail.com>
2022-10-04 17:04:42 -0600

    commit tree 40473b0805921d2d58f6f28ba03909cd4a757892

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your code here!
+// practice 
+const mainHead = document.getElementById("main")
+mainHead.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML = "Kyle is the champion"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Write your code here!
+// const newHeader = document.createElement("h1")
+// newHeader.id = "victory"
+// newHeader.innerHTML = "Kyle is the champion"
